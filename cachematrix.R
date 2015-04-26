909b2149d916ec9f620cc254f17f11f1060cd1ab
renayssanceman <sriram16@gmail.com>
2015-04-25 19:17:22 -0700

    Coursera Programming Assignment 2 by Sriram AKA renayssanceman

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix is used for scoping 
+## cacheSolve is used to compute the inverse of the matrix
 
-## Write a short comment describing this function
+## The following function is used to cache the inverse of
+## the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(inverse) m <<- inverse
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## the following function is used to compute the inverse 
+## of the matrix and also checks if inverse is in cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+         m <- x$getmatrix()
+	  if(!is.null(m)) {
+	    message("getting cached data")
+	    return(m)
+	  }
+	  data <- x$get()
+	  m <- solve(data, ...)
+	  x$setmatrix(m)
+  m
 }
